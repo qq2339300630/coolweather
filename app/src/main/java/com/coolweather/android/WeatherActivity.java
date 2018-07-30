5f89e99c735d05ff25449024f66f581862daad95
Yanqing <2339300630@qq.com>
2018-07-30 11:20:05 +0800

    后天更新

--- app/src/main/java/com/coolweather/android/WeatherActivity.java
+++ app/src/main/java/com/coolweather/android/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.coolweather.android;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v4.view.GravityCompat;
@@ -20,6 +21,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.coolweather.android.gson.Forecast;
 import com.coolweather.android.gson.Weather;
+import com.coolweather.android.service.AutoUpdateService;
 import com.coolweather.android.util.HttpUtil;
 import com.coolweather.android.util.Utility;
 
@@ -181,6 +183,8 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText( carWath );
         sportText.setText( sport );
         weatherLayout.setVisibility( View.VISIBLE );
+         Intent intent = new Intent( this, AutoUpdateService.class );
+         startService( intent );
      }
 
      private void loadBingPic(){
